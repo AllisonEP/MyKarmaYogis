9ad78e8e57bbad326a904a5a30c58017335d53c9
Allison Perry <allisonpshortnorth@gmail.com>
2021-02-23 12:40:30 -0500

    'new volunteer view and route established'

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,11 +1,25 @@
 class VolunteersController < ApplicationController
     def new
+        @volunteer = Volunteer.new
     end
-    
+
     def create
+       @volunteer = Volunteer.new(volunteer_params)
+       if @volunteer.save
+        session[:volunteer_id] = @volunteer_id
+        redirect_to @volunteer
+       else
+        render :new
+       end
     end 
 
     def show
     end
+
+    private 
+
+    def volunteer_params
+        params.require(:volunteer).permit(:username, :email, :password)
+    end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+
+
+  get '/signup' => 'volunteers#new'
+  post '/signup' => 'volunteers#create'
+
+
+
   resources :departments
   resources :teams
   resources :volunteers
   resources :projects
   #may delete
-  get '/signup' => 'volunteers#new'
- #post '/signup' => 'volunteers#create'
-
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
