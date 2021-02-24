480161f576bcdc39eee4249b5f556ea7c20d73c6
Allison Perry <allisonpshortnorth@gmail.com>
2021-02-24 10:30:49 -0500

    'created scope method'

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,9 @@
 class Project < ApplicationRecord
   belongs_to :volunteer
-  belongs_to :department
   has_many :teams
   has_many :volunteers, through: :teams
+
+  def self.find_latest_five
+    order(id: :desc).limit(5)
+  end
 end
